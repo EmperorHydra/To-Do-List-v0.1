9cbf5f3279d0521b518d40919f1452a3e93a650d
EmperorHydra <136229991+EmperorHydra@users.noreply.github.com>
2026-06-13 01:54:30 +0800

    to_do_list 0.2

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 print("To Do App")
 
+
 print("Do you wish to add a task?")
 option = input("if yes input y, if no input n:")
 if option == "y":
-    input_task_1 = input("Input Task 1: ")
-    print(input_task_1)
-    input_task_2 = input("Input Task 2: ")
-    print(input_task_2)
+    task_1 = input("Input Task 1: ")
+    print(task_1)
+    task_2 = input("Input Task 2: ")
+    print(task_2)
+
+    to_do_list = {task_1, task_2}
+    print(to_do_list)
     print("This is your to do list:")
-    print(input_task_1)
-    print(input_task_2)
+    print(task_1)
+    print(task_2)
 else:
-    print("Thank you,this is your to do list for the day,it is currently empty please restart to fill")
+    print("Thank you,this is your to do list for the day.")
